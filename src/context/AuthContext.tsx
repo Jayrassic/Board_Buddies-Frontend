021a40f656075ce09bd70ec1f6d352dbadf349aa
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-05 16:42:58 -0500

    Created authorization context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,50 @@
+import { FC, ReactNode, createContext, useEffect, useReducer } from "react";
+
+interface UserInterface {
+  user: object;
+}
+
+interface NullInterface {
+  user: null;
+}
+
+const initialState = { user: null };
+
+type StateType = { user: null } | { user: UserInterface };
+
+type ACTIONTYPE =
+  | { type: "LOGIN"; payload: UserInterface }
+  | { type: "LOGOUT"; payload: NullInterface };
+
+export const AuthContext = createContext(initialState);
+
+export function authReducer(state: typeof initialState, action: ACTIONTYPE) {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT":
+      return { user: null };
+    default:
+      return state;
+  }
+}
+
+type ChildrenProps = { children: React.ReactNode };
+
+export const AuthContextProvider = ({ children }: ChildrenProps) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
