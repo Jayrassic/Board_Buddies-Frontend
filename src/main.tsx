94c8ee5ec393853a27f5f3d0386dcac3ee0ddb74
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-07 12:09:43 -0500

    Added GamesContexProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { GameContextProvider } from "./context/GamesContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <GameContextProvider>
+        <App />
+      </GameContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
