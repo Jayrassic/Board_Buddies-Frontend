f6935c9ba0a9fcd699f7109975c9cca4cf185baf
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-13 12:40:31 -0500

    Created global types

---
+++ src/models/global.tsx
@@ -0,0 +1,23 @@
+export type UsersType = {
+  _id: string;
+  userName: string;
+  email: string;
+  hash: string;
+  admin: boolean;
+};
+
+export type GamesType = {
+  _id: string;
+  owner: UsersType;
+  name: string;
+  description: string;
+  minPlayers: number;
+  maxPlayers: number;
+  image: string;
+  thumbnail: string;
+};
+
+export type UserTokenType = {
+  email: string;
+  token: string;
+};
