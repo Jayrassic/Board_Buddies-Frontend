f0e89f8827b1992f1f4747c82b3e04635db26681
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-13 12:49:39 -0500

    Created sort Reducer

---
+++ src/utils/sortReducer.ts
@@ -0,0 +1,33 @@
+import * as sortFunctions from "../utils/sortFunctions";
+import { GamesType } from "../models/global";
+
+const initialState: GamesType[] = [];
+
+type ActionType = { type: string; payload: GamesType[] };
+
+export default function sortReducer(
+  state: typeof initialState,
+  action: ActionType
+) {
+  switch (action.type) {
+    case "Game Ascend":
+      return sortFunctions.sortGameNameAscending(action.payload);
+    case "Game Descend":
+      return sortFunctions.sortGameNameDescending(action.payload);
+    case "Owner Ascend":
+      return sortFunctions.sortGameOwnerAscending(action.payload);
+    case "Owner Descend":
+      return sortFunctions.sortGameOwnerDescending(action.payload);
+    case "Min Ascend":
+      return sortFunctions.sortMinPlayersAscending(action.payload);
+    case "Min Descend":
+      return sortFunctions.sortMinPlayersDescending(action.payload);
+    case "Max Ascend":
+      return sortFunctions.sortMaxPlayersAscending(action.payload);
+    case "Max Descend":
+      return sortFunctions.sortMaxPlayersDescending(action.payload);
+
+    default:
+      return state;
+  }
+}
