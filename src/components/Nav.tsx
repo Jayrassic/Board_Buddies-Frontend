f4df1a59416686da63db4e2c4f9e5cd657102944
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-02-29 14:32:35 -0500

    Added Bootstrap stylings

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,26 +11,29 @@ export default function Nav(): React.JSX.Element {
   }
 
   return (
-    <header>
-      <Link to="/">Board buddies</Link>
-      <nav>
-        {user && (
-          <>
-            <p>Welcome {user.email}</p>
-            <Link to="/" onClick={logoutHandler}>
-              LogOut
-            </Link>
-            <Link to={`/user/${user.userName}`}>Your Games</Link>
-          </>
-        )}
+    <nav className="navbar">
+      <Link className="nav-link navbar-brand" to="/">
+        Board buddies
+      </Link>
+      {user && (
+        <div className="d-flex align-items-center gap-4">
+          <span className="nav-item navbar-text">Welcome {user.email}</span>
 
-        {!user && (
-          <>
-            <Link to="/create_account">Create Account</Link>
-            <Link to="/login">Log In</Link>
-          </>
-        )}
-      </nav>
-    </header>
+          <Link className="nav-link" to="/" onClick={logoutHandler}>
+            LogOut
+          </Link>
+          <Link className="nav-link" to={`/user/${user.userName}`}>
+            Your Games
+          </Link>
+        </div>
+      )}
+
+      {!user && (
+        <div>
+          <Link to="/create_account">Create Account</Link>
+          <Link to="/login">Log In</Link>
+        </div>
+      )}
+    </nav>
   );
 }
