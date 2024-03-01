7882d7abd31b77820a58f87020cca3d95a5c38b6
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-01 15:50:48 -0500

    Added confermation modal

--- src/components/GameTable.tsx
+++ src/components/GameTable.tsx
@@ -2,6 +2,7 @@ import { GamesType } from "../models/global";
 import { useGamesContext } from "../hooks/useGamesContext";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 export default function GameTable({
   data,
@@ -11,6 +12,8 @@ export default function GameTable({
   const { user } = useAuthContext();
   const { dispatch } = useGamesContext();
   const { id } = useParams();
+
+  const [deleteData, setDeleteData] = useState(null);
 
   const handleClick = async (data) => {
     console.log(data);
@@ -30,35 +33,91 @@ export default function GameTable({
   };
 
   return (
-    <table className=" table table-striped table-bordered">
-      <thead className="table-dark">
-        <tr>
-          <th>Game</th>
-          {!id && <th className="col-2">Owner</th>}
-          <th className="col-2">Min Players</th>
-          <th className="col-2">Max Players</th>
-          {id && <th className="col-1">Delete</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((game) => {
-          return (
-            <>
-              <tr key={game._id + game.owner._id}>
-                <td>{game.name}</td>
-                {!id && <td>{game.owner.userName}</td>}
-                <td>{game.minPlayers}</td>
-                <td>{game.maxPlayers}</td>
-                {user && id && (
-                  <td>
-                    <button onClick={() => handleClick(game)}>Delete</button>
-                  </td>
-                )}
-              </tr>
-            </>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className=" table table-striped table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th>Game</th>
+            {!id && <th className="col-2">Owner</th>}
+            <th className="col-2">Min Players</th>
+            <th className="col-2">Max Players</th>
+            {id && <th className="col-1">Delete</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((game) => {
+            return (
+              <>
+                <tr key={game._id + game.owner._id}>
+                  <td>{game.name}</td>
+                  {!id && <td>{game.owner.userName}</td>}
+                  <td>{game.minPlayers}</td>
+                  <td>{game.maxPlayers}</td>
+                  {user && id && (
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop"
+                        onClick={() => setDeleteData(game)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  )}
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Are you sure you want to remove this game from you collection?
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              You can always add the game back if you change your mind.
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                No
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+                onClick={() => handleClick(deleteData)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/views/GameDetails.tsx
+++ src/views/GameDetails.tsx
@@ -110,9 +110,13 @@ export default function GameDetails() {
           </div>
           {user && (
             <button
-              className="btn btn-primary btn-lg"
               type="button"
-              onClick={(e) => handleAdd(e)}
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+              // className="btn btn-primary btn-lg"
+              // type="button"
+              // onClick={(e) => handleAdd(e)}
             >
               Add Game to your list
             </button>
@@ -120,6 +124,53 @@ export default function GameDetails() {
           {error && <h2>{error}</h2>}
         </div>
       )}
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Add Game To your Collection?
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              {" "}
+              Are You sure you want to add this game?
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                No
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={(e) => handleAdd(e)}
+                data-bs-dismiss="modal"
+              >
+                Yes!
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
