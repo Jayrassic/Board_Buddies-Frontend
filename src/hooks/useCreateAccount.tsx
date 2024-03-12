5a74bf0deff839120ebfccc44b94ce6b94646add
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-12 13:41:37 -0400

    Changed error type

--- src/hooks/useCreateAccount.tsx
+++ src/hooks/useCreateAccount.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
 export function useCreateAccount() {
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { dispatch } = useAuthContext();
 
@@ -14,22 +14,27 @@ export function useCreateAccount() {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:3000/users/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password, userName }),
-    });
-    const json = await response.json();
+    try {
+      const response = await fetch("http://localhost:3000/users/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password, userName }),
+      });
+      const json = await response.json();
 
-    if (!response.ok) {
-      setIsLoading(false);
-      setError(json.error);
-    }
+      if (!response.ok) {
+        setIsLoading(false);
+        setError([json.error]);
+      }
 
-    if (response.ok) {
-      localStorage.setItem("user", JSON.stringify(json));
-      dispatch({ type: "LOGIN", payload: json });
+      if (response.ok) {
+        localStorage.setItem("user", JSON.stringify(json));
+        dispatch({ type: "LOGIN", payload: json });
+        setIsLoading(false);
+      }
+    } catch (err) {
       setIsLoading(false);
+      setError([(err as Error).message]);
     }
   }
   return { createAccount, isLoading, error };
--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
 export function useLogin() {
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { dispatch } = useAuthContext();
 
@@ -10,22 +10,28 @@ export function useLogin() {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:3000/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
-    const json = await response.json();
+    try {
+      const response = await fetch("http://localhost:3000/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      const json = await response.json();
 
-    if (!response.ok) {
-      setIsLoading(false);
-      setError(json.error);
-    }
+      if (!response.ok) {
+        setIsLoading(false);
+        console.log("json");
+        setError(json.error);
+      }
 
-    if (response.ok) {
-      localStorage.setItem("user", JSON.stringify(json));
-      dispatch({ type: "LOGIN", payload: json });
+      if (response.ok) {
+        localStorage.setItem("user", JSON.stringify(json));
+        dispatch({ type: "LOGIN", payload: json });
+        setIsLoading(false);
+      }
+    } catch (err) {
       setIsLoading(false);
+      setError([(err as Error).message]);
     }
   }
 
