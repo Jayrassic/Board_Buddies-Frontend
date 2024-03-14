16aaf378f5b6d8d2265bc711d3637992f0026b92
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-13 23:50:26 -0400

    Fixed key warning

--- src/components/GameTable.tsx
+++ src/components/GameTable.tsx
@@ -52,38 +52,36 @@ export default function GameTable({
         <tbody>
           {data.map((game) => {
             return (
-              <>
-                <tr key={game._id + game.owner._id}>
-                  <td>{game.name}</td>
-                  {!id && <td>{game.owner.userName}</td>}
-                  <td>{game.minPlayers}</td>
-                  <td>{game.maxPlayers}</td>
-                  <td>{game.playingTime}</td>
-                  {user && id && (
-                    <td>
-                      <button
-                        type="button"
-                        className="btn"
-                        data-bs-toggle="modal"
-                        data-bs-target="#staticBackdrop"
-                        onClick={() => setDeleteData(game)}
+              <tr key={game._id + game.owner._id}>
+                <td>{game.name}</td>
+                {!id && <td>{game.owner.userName}</td>}
+                <td>{game.minPlayers}</td>
+                <td>{game.maxPlayers}</td>
+                <td>{game.playingTime}</td>
+                {user && id && (
+                  <td>
+                    <button
+                      type="button"
+                      className="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#staticBackdrop"
+                      onClick={() => setDeleteData(game)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="20"
+                        viewBox="0 -960 960 960"
+                        width="20"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          height="20"
-                          viewBox="0 -960 960 960"
-                          width="20"
-                        >
-                          <path
-                            fill="#dc3545"
-                            d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"
-                          />
-                        </svg>
-                      </button>
-                    </td>
-                  )}
-                </tr>
-              </>
+                        <path
+                          fill="#dc3545"
+                          d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"
+                        />
+                      </svg>
+                    </button>
+                  </td>
+                )}
+              </tr>
             );
           })}
         </tbody>
