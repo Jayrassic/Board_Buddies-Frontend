dbea22ab4ea6c507cccbb13eac61ce44acdf1ebc
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-14 19:35:10 -0400

    Made Nav responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,35 +11,63 @@ export default function Nav(): React.JSX.Element {
   }
 
   return (
-    <nav className="navbar bg-primary text-light p-3">
-      <Link className="nav-link navbar-brand text-white" to="/">
-        Board buddies
-      </Link>
-      {user && (
-        <div className="d-flex align-items-center gap-4">
-          <span className="nav-item navbar-text text-white fs-3">
-            Welcome {user.userName}
-          </span>
+    <nav
+      className="navbar bg-primary navbar-primary p-3 navbar-expand-md"
+      data-bs-theme="dark"
+    >
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Board buddies
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <Link className="nav-link" to="/" onClick={logoutHandler}>
-            LogOut
-          </Link>
-          <Link className="nav-link" to={`/user/${user.userName}`}>
-            Your Games
-          </Link>
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          {user && (
+            // <div className="d-flex align-items-center gap-4">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <p className=" navbar-text text-white m-0 pe-4">
+                  Welcome {user.userName}
+                </p>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link " to="/" onClick={logoutHandler}>
+                  LogOut
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link " to={`/user/${user.userName}`}>
+                  Your Games
+                </Link>
+              </li>
+            </ul>
+          )}
+          {!user && (
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/create_account">
+                  Create Account
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Log In
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
-      )}
-
-      {!user && (
-        <div className="d-flex align-items-center gap-4">
-          <Link className="nav-link" to="/create_account">
-            Create Account
-          </Link>
-          <Link className="nav-link" to="/login">
-            Log In
-          </Link>
-        </div>
-      )}
+      </div>
     </nav>
   );
 }
