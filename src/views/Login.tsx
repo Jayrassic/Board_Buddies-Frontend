bbc6e2e18824619400ef06f423b2b5d1102300bd
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-03-21 13:04:58 -0400

    Removed login email character limit

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -24,8 +24,6 @@ export default function LoginPage(): React.JSX.Element {
               id="email"
               className="form-control"
               type="email"
-              minLength={2}
-              maxLength={15}
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               autoComplete="on"
